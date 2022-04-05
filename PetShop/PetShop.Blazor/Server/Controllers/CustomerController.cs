c9d3fafd090e650cbe0c9d9200d9b966ce0cfb45
Evangelos Mandalis <evanmand@ea.gr>
2022-04-05 14:43:05 +0300

    Created Customer Controller and CustomerListViewModel

---
+++ PetShop/PetShop.Blazor/Server/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Blazor.Shared;
+using PetShop.EF.Repos;
+using PetShop.Model;
+
+namespace PetShop.Blazor.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController: ControllerBase
+    {
+        private readonly IEntityRepo<Customer> _customerRepo;
+
+        public CustomerController(IEntityRepo<Customer> customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<CustomerListViewModel>> Get()
+        {
+
+        }
+        [HttpPost]
+        public async Task Post(CustomerListViewModel customer)
+        {
+
+        }
+        [HttpDelete("{id}")]
+        public async Task Delete(Guid id)
+        {
+
+        }
+        [HttpPut]
+        public async Task<ActionResult> Put(CustomerListViewModel customer)
+        {
+
+        }
+    }
+}
---
+++ PetShop/PetShop.Blazor/Shared/CustomerListViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Blazor.Shared
+{
+    public class CustomerListViewModel
+    {
+        public Guid Id { get; set; } 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string FullName { get; set; } 
+        public string Phone { get; set; }
+        public string TIN { get; set; }
+    }
+}
