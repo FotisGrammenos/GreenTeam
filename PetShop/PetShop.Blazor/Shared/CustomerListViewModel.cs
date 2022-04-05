ef4df8aeb34e1de580d1fcc2e90535e40fbe82c0
Evangelos Mandalis <evanmand@ea.gr>
2022-04-05 18:22:14 +0300

    adeed

--- PetShop/PetShop.Blazor/Shared/CustomerListViewModel.cs
+++ PetShop/PetShop.Blazor/Shared/CustomerListViewModel.cs
@@ -11,7 +11,6 @@ namespace PetShop.Blazor.Shared
         public Guid Id { get; set; } 
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string FullName { get; set; } 
         public string Phone { get; set; }
         public string TIN { get; set; }
     }
