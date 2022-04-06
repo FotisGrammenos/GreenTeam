aeb7e59aaeb2b6ce81f6e2a70fff0cfbdb34e40d
Evangelos Mandalis <evanmand@ea.gr>
2022-04-07 00:19:20 +0300

    Added Features to Transaction

--- PetShop/PetShop.Blazor/Server/Controllers/TransactionController.cs
+++ PetShop/PetShop.Blazor/Server/Controllers/TransactionController.cs
@@ -1,67 +1,70 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using PetShop.Blazor.Shared;
-//using PetShop.EF.Repos;
-//using PetShop.Model;
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Blazor.Shared;
+using PetShop.EF.Repos;
+using PetShop.Model;
 
-//namespace PetShop.Blazor.Server.Controllers
-//{
-//    [ApiController]
-//    [Route("[controller]")]
-//    public class TransactionController
-//    {
-//        private readonly IEntityRepo<Transaction> _transactionRepo;
-//        public TransactionController(IEntityRepo<Transaction> transactionRepo)
-//        {
-//            _transactionRepo = transactionRepo;
-//        }
-//        [HttpGet]
-//        public async Task<IEnumerable<TransactionListViewModel>> Get()
-//        {
-//            var result = await _transactionRepo.GetAllAsync();
-//            return result.Select(x => new TransactionListViewModel
-//            {
-//                ID = x.ID,
-//                Date = x.Date,
-//                CustomerID = x.CustomerID,
-//                CustomerName = x.Customer.Name,
-//                EmployeeID = x.EmployeeID,
-//                EmployeeName = x.Employee.Name,
-//                PetId = x.PetID,
-//                PetPrice = x.PetPrice,
-//                PetFoodID = x.PetFoodID,
-//                PetFoodQty = x.PetFoodQty,
-//                PetFoodPrice=x.PetFoodPrice,
-//                TotalPrice = x.TotalPrice,
-//            });
-//        }
-//        public async Task<TransactionListViewModel> Get(Guid id)
-//        {
-//            TransactionListViewModel model = new();
-//            if (id != Guid.Empty)
-//            {
-//                var existing = await _transactionRepo.GetByIdAsync(id);
-//                model.ID = existing.ID;
-//                model.Date = existing.Date;
-//                model.CustomerID = existing.CustomerID;
-//                model.CustomerName = existing.Customer.Name;
-//                model.EmployeeID = existing.EmployeeID;
-//                model.EmployeeName = existing.Employee.Name;
-//                model.PetId = existing.PetID;
-//                model.PetPrice = existing.PetPrice;
-//                model.PetFoodID = existing.PetFoodID;
-//                model.PetFoodQty = existing.PetFoodQty;
-//                model.PetFoodPrice = existing.PetFoodPrice;
-//                model.TotalPrice = existing.TotalPrice;
-//            }
+namespace PetShop.Blazor.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController
+    {
+        private readonly IEntityRepo<Transaction> _transactionRepo;
+        public TransactionController(IEntityRepo<Transaction> transactionRepo)
+        {
+            _transactionRepo = transactionRepo;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<TransactionListViewModel>> Get()
+        {
+            var result = await _transactionRepo.GetAllAsync();
+            return result.Select(x => new TransactionListViewModel
+            {
+                ID = x.ID,
+                Date = x.Date,
+                CustomerName = x.Customer.Name + x.Customer.Surname,
+                EmployeeName = x.Employee.Name + x.Customer.Surname,
+                PetPrice = x.Pet.Price,
+                PetFoodQty = x.PetFoodQty,
+                PetFoodPrice = x.PetFood.Price,
+                TotalPrice = x.TotalPrice,
+            });
+        }
+        public async Task<TransactionEditViewModel> Get(Guid id)
+        {
+            TransactionEditViewModel model = new();
+            if (id != Guid.Empty)
+            {
+                var existing = await _transactionRepo.GetByIdAsync(id);
+                model.ID = existing.ID;
+                model.Date = existing.Date;
+                model.CustomerName = existing.Customer.Name + existing.Customer.Surname;
+                model.EmployeeName = existing.Employee.Name + existing.Customer.Surname;
+                model.PetPrice = existing.Pet.Price;
+                model.PetFoodQty = existing.PetFoodQty;
+                model.PetFoodPrice = existing.PetFood.Price;
+                model.TotalPrice = existing.TotalPrice;
+            }
 
-//            return model;
-//        }
-//        [HttpPost]
-//        public async Task Post(TransactionListViewModel transaction)
-//        {
-//            var newTransaction = new Transaction(transaction.Name, customer.Surname, customer.Phone, customer.TIN);
+            return model;
+        }
+        [HttpPost]
+        public async Task Post(TransactionEditViewModel transaction)
+        {
+            var newTransaction = new Transaction()
+            {
+                Date= transaction.Date,
+                CustomerID = transaction.CustomerID,
+                EmployeeID = transaction.EmployeeID,
+                PetID = transaction.PetID,
+                PetPrice = transaction.PetPrice,
+                PetFoodID = transaction.PetFoodID,
+                PetFoodQty = transaction.PetFoodQty,
+                PetFoodPrice = transaction.PetFoodPrice,
+                TotalPrice = transaction.TotalPrice,
+            };
 
-//            await _customerRepo.AddAsync(newCustomer);
-//        }
-//    }
-//}
+            await _transactionRepo.AddAsync(newTransaction);
+        }
+    }
+}
--- PetShop/PetShop.Blazor/Shared/TransactionListViewModel.cs
+++ PetShop/PetShop.Blazor/Shared/TransactionListViewModel.cs
@@ -10,11 +10,24 @@ namespace PetShop.Blazor.Shared
     {
         public Guid ID { get; set; }
         public DateTime Date { get; set; }= DateTime.Now;
+        public string Breed { get; set; }
+        public string CustomerName { get; set; }
+        public string EmployeeName { get; set; }
+        public decimal PetPrice { get; set; }
+        public int PetFoodQty { get; set; }
+        public decimal PetFoodPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+
+    }
+    public class TransactionEditViewModel
+    {
+        public Guid ID { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
         public Guid CustomerID { get; set; }
         public string CustomerName { get; set; }
         public Guid EmployeeID { get; set; }
         public string EmployeeName { get; set; }
-        public Guid PetId { get; set; }
+        public Guid PetID { get; set; }
         public decimal PetPrice { get; set; }
         public Guid PetFoodID { get; set; }
         public int PetFoodQty { get; set; }
