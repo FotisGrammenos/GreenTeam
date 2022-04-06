83620a25cbad2fe1a649fff5dcdb4b0913fd202b
Evangelos Mandalis <evanmand@ea.gr>
2022-04-06 23:16:29 +0300

    Adeed Transactions

---
+++ PetShop/PetShop.Blazor/Shared/TransactionListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Blazor.Shared
+{
+    public class TransactionListViewModel
+    {
+        public Guid ID { get; set; }
+        public DateTime Date { get; set; }= DateTime.Now;
+        public Guid CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public Guid EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public Guid PetId { get; set; }
+        public decimal PetPrice { get; set; }
+        public Guid PetFoodID { get; set; }
+        public int PetFoodQty { get; set; }
+        public decimal PetFoodPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+
+    }
+}
+// Date, CustomerID, EmployeeID, PetID, PetPrice,
+//PetFoodID, PetFoodQty, PetFoodPrice, Total Price)
