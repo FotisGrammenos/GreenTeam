2f18a047d598105295f5afc09e70d97d157be411
Evangelos Mandalis <evanmand@ea.gr>
2022-04-06 16:35:26 +0300

    Added customer edit forms

--- PetShop/PetShop.Blazor/Shared/CustomerListViewModel.cs
+++ PetShop/PetShop.Blazor/Shared/CustomerListViewModel.cs
@@ -14,4 +14,12 @@ namespace PetShop.Blazor.Shared
         public string Phone { get; set; }
         public string TIN { get; set; }
     }
+    public class CustomerEditViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string TIN { get; set; }
+    }
 }
